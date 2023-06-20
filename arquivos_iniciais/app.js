b9e267a99d5ab237c51ac10239d656cdceb8e84f
Hugo Coutinho <109985122+hugocoutinhoo@users.noreply.github.com>
2023-06-19 21:06:57 -0300

    diminuindo o tamanho do código

--- arquivos_iniciais/app.js
+++ arquivos_iniciais/app.js
@@ -65,9 +65,23 @@ function cadastrarDespesa() {
 
 	if(despesa.validarDados()) {
 		bd.gravar(despesa)
-		$('#sucessoGravacao').modal('show')
+
+		document.getElementById('modal_titulo').innerHTML = 'Registro inserido com sucesso'
+		document.getElementById('modal_titulo_div').className = 'modal-header text-success'
+		document.getElementById('modal_conteudo').innerHTML = 'Despesa cadastrada com sucesso.'
+		document.getElementById('modal_btn').innerHTML = 'Voltar'
+		document.getElementById('modal_btn').className = 'btn btn-success'
+		
+		$('#modalRegistraDespesa').modal('show')
 	} else {
-		$('#erroGravacao').modal('show')
+
+		document.getElementById('modal_titulo').innerHTML = 'Erro na inclusão do registro.'
+		document.getElementById('modal_titulo_div').className = 'modal-header text-danger'
+		document.getElementById('modal_conteudo').innerHTML = 'Erro na gravação, verifique se todos os campos foram preenchidos corretamente.'
+		document.getElementById('modal_btn').innerHTML = 'Voltar e corrigir'
+		document.getElementById('modal_btn').className = 'btn btn-danger'
+
+		$('#modalRegistraDespesa').modal('show')
 	}
 	
 }
